3e2baa89f632602dbf91ceb34d2bfbc5290ecd4d
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-08-31 22:40:57 -0500

    2.5.3

--- 2-5-CSS-Variables/scripts.js
+++ 2-5-CSS-Variables/scripts.js
@@ -27,3 +27,47 @@ addEventListener('scroll', () => {
     rootStyle.setProperty('--width',sc);
     console.log(sc);
 })
+
+
+/* FONDO RGB*/
+
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+const rootStyless = document.documentElement.style;
+
+const changeColorRed = e =>{
+    rootStyless.setProperty('--red', e.target.value)   
+}
+
+const changeColorGreen = e =>{
+    rootStyless.setProperty('--green', e.target.value)   
+}
+
+const changeColorBlue = e =>{
+    rootStyless.setProperty('--blue', e.target.value)   
+}
+
+red.addEventListener('change', e => {
+    changeColorRed(e)
+})
+
+red.addEventListener('mousemove', e => {
+    changeColorRed(e)
+})
+
+green.addEventListener('change', e => {
+    changeColorGreen(e)
+})
+
+green.addEventListener('mousemove', e => {
+    changeColorGreen(e)
+})
+
+blue.addEventListener('change', e => {
+    changeColorBlue(e)
+})
+
+blue.addEventListener('mousemove', e => {
+    changeColorBlue(e)
+})
