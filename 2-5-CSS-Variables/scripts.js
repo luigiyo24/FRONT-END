80859f39aa461cd6bea65e16da26fe7aa0091e12
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-08-31 22:20:09 -0500

    2.5.2

--- 2-5-CSS-Variables/scripts.js
+++ 2-5-CSS-Variables/scripts.js
@@ -11,4 +11,19 @@ document.documentElement
 */
 
 const rootStyles = getComputedStyle(document.documentElement);
-console.log(rootStyles);
+// console.log(rootStyles);
+
+const h1 = document.documentElement.scrollHeight;
+console.log(h1);
+const h2 = document.documentElement.clientHeight;
+console.log(h2);
+console.log(h1-h2);
+
+const scrollUnit = (h1 - h2)/100;
+const rootStyle = document.documentElement.style;
+
+addEventListener('scroll', () => {
+    const sc = Math.round(scrollY / scrollUnit);
+    rootStyle.setProperty('--width',sc);
+    console.log(sc);
+})
