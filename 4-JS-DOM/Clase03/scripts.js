57f89b26e3fd379cc68b985f83f865f36eb7f7f8
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-09-03 23:39:12 -0500

    4.0

---
+++ 4-JS-DOM/Clase03/scripts.js
@@ -0,0 +1,7 @@
+const parent = document.getElementById("parent");
+
+console.log(parent);
+console.log(parent.children);
+console.log(parent.firstElementChild);
+console.log(parent.lastElementChild);
+console.log(parent.hasChildNodes());
